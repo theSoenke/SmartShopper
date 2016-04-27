101b5d76b7555a0583b03060deb7cbb117c8e6e7
theSoenke <soenke.behrendt@outlook.com>
2016-04-27 12:49:38 +0200

    Add mongodb to setup

--- Backend/setup.sh
+++ Backend/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-apt -y update
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-apt install -y nodejs
-npm install pm2 -g
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+apt update
+apt install -y nodejs mongodb-org
 
 # create new user to run app
 useradd -mrU web
@@ -13,11 +15,14 @@ chgrp web /var/www
 cd /var/www/
 su web
 
-git clone https://github.com/theSoenke/SmartShopper
-cd SmartShopper/Backend
 
-pm2 start app.js -i 0 --name "api"
-pm2 startup systemd
+#git clone https://github.com/theSoenke/SmartShopper
+#cd SmartShopper/Backend
+#npm install
+#npm install pm2 -g
+
+#pm2 start app.js -i 0 --name "api"
+#pm2 startup systemd
 
 # pm2 reload api
 # pm2 stop api
