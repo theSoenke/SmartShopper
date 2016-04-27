df5776477f4a7fa10242b4b3a77f35ebb96bf2f6
theSoenke <soenke.behrendt@outlook.com>
2016-04-27 16:22:55 +0200

    Mongodb systemd service

--- Backend/setup.sh
+++ Backend/setup.sh
@@ -1,25 +1,49 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+set -ex
 
 apt update
-apt install -y nodejs mongodb-org
+apt install -y curl git
+curl -sL https://deb.nodesource.com/setup_5.x | bash -
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+apt update
+apt install -y nodejs
+apt install -y mongodb-org
 npm install pm2 -g
 
-#git clone https://github.com/theSoenke/SmartShopper
-mv /root/SmartShopper /var/www/
-cd /var/www/SmartShopper/Backend
-npm install
+
+# setup mongodb
+echo '[Unit]
+Description=High-performance, schema-free document-oriented database
+
+[Service]
+User=mongod
+Group=mongod
+OPTIONS=--quiet -f /etc/mongod.conf
+ExecStart=/usr/bin/mongod $OPTIONS run
+PIDFile=/var/run/mongodb/mongod.pid
+
+[Install]
+WantedBy=multi-user.target' > /lib/systemd/system/mongod.service
+
+systemctl daemon-reload
+systemctl enable mongod
+systemctl start mongod
+
 
 # create new user to run app
 useradd -mrU web
 mkdir -p /var/www
 chown web /var/www
 chgrp web /var/www
-su web
 
+mv ~/SmartShopper /var/www/
+cd /var/www/SmartShopper/Backend
+npm install
+
+su web
 pm2 start app.js -i 0 --name "api"
 pm2 startup systemd
 
