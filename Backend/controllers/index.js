7c8fa4193f9bdb024775e4845771f05b53e8cce6
theSoenke <soenke.behrendt@outlook.com>
2016-04-27 11:08:36 +0200

    Start implementing database

--- Backend/controllers/index.js
+++ Backend/controllers/index.js
@@ -1,8 +1,27 @@
 var express = require('express')
 var router = express.Router()
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test')
+var Product = require('../models/product')
 
 router.get('/', function (req, res, next) {
+  createProduct()
   res.json('test')
 })
+
+function createProduct () {
+  var kaese = new Product({
+    name: 'gouda'
+  })
+
+  kaese.save(function (err) {
+    if (err) {
+      throw err
+    }
+
+    console.log('product saved')
+  })
+  console.log('test');
+}
 
 module.exports = router
