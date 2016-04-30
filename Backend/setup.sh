4b42ffdcc1bcb76414222b5056ffeafdf121caff
theSoenke <soenke.behrendt@outlook.com>
2016-04-30 10:54:04 +0200

    Update to Node 6

--- Backend/setup.sh
+++ Backend/setup.sh
@@ -4,7 +4,7 @@ set -ex
 
 apt update
 apt install -y curl git
-curl -sL https://deb.nodesource.com/setup_5.x | bash -
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
