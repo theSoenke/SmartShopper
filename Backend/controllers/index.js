3e5cd6c737127fc870b801bdbb611058ded29e13
theSoenke <soenke.behrendt@outlook.com>
2016-04-30 10:52:28 +0200

    List api basics

--- Backend/controllers/index.js
+++ Backend/controllers/index.js
@@ -1,41 +1,25 @@
+'use strict'
+
 var express = require('express')
-var mongoose = require('mongoose')
 
 var router = express.Router()
-mongoose.connect('mongodb://localhost/test')
-
-var Product = require('../models/product')
-// var Market = require('../models/market')
-// var Shelf = require('../models/shelf')
+router.use(require('./lists'))
 
 router.get('/', function (req, res) {
-  res.json('test')
+  res.send('Server running')
 })
 
+// products
 router.get('/products/:market', function (req, res) {
   var market = req.params.market
   res.json(market)
 })
 
-router.get('/search/:market/:query'), function (req, res) {
-  var market = req.params.market
+// search
+router.get('/search/:market/:query', function (req, res) {
+  // var market = req.params.market
   var query = req.params.query
-
   res.json(query)
-}
-
-function createProduct (productName) {
-  var kaese = new Product({
-    name: productName
-  })
-
-  kaese.save(function (err) {
-    if (err) {
-      throw err
-    }
-
-    console.log('product saved')
-  })
-}
+})
 
 module.exports = router
