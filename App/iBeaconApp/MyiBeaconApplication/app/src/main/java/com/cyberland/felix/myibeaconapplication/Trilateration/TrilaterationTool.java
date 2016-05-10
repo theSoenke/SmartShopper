9dd2a9babaa8a652d2e43a2c9fd249be4596d4fd
ChrisMarxDev <dev@christopher-marx.de>
2016-05-10 15:47:33 +0200

    Heuristische lokalisierung, Code verbessert

--- App/iBeaconApp/MyiBeaconApplication/app/src/main/java/com/cyberland/felix/myibeaconapplication/Trilateration/TrilaterationTool.java
+++ App/iBeaconApp/MyiBeaconApplication/app/src/main/java/com/cyberland/felix/myibeaconapplication/Trilateration/TrilaterationTool.java
@@ -21,6 +21,8 @@ public class TrilaterationTool {
 
     public Map<Integer, Vector> beaconMap;
 
+    List<BeaconEntity> beacons;
+
 
     Vector lastTrilateration;
     List<Vector> positions;
@@ -28,15 +30,22 @@ public class TrilaterationTool {
 
     public TrilaterationTool()
     {
-        p1 = new Vector(6.5, 0);
-        p2 = new Vector(7, 4.8);
-        p3 = new Vector(0, 4.8);
-        p4 = new Vector(0, 0);
+        p1 = new Vector(0, 0);
+        p2 = new Vector(4.8, 0);
+        p3 = new Vector(4.8, 7);
+        p4 = new Vector(0, 7);
 
         beaconID1 = 24286; //Felix Beacon esti 008
         beaconID2 = 1744; //esti003
         beaconID3 = 21333; //esti005
-        beaconID4 = 41230; //elo e10
+        beaconID4 = 31883; //esti002
+
+        beacons = new ArrayList<>();
+        beacons.add(new BeaconEntity(p1, beaconID1,1));
+        beacons.add(new BeaconEntity(p2, beaconID2,2));
+        beacons.add(new BeaconEntity(p3, beaconID3,3));
+        beacons.add(new BeaconEntity(p4, beaconID4,4));
+
 
         beaconMap = new HashMap<>();
         beaconMap.put(beaconID1, p1);
@@ -47,45 +56,43 @@ public class TrilaterationTool {
         lastTrilateration = new Vector(0, 0);
 
         positions = new ArrayList<Vector>(4);
-        positions.add(0,p1);
-        positions.add(1,p2);
-        positions.add(2,p3);
-        positions.add(3,p4);
+        positions.add(0, p1);
+        positions.add(1, p2);
+        positions.add(2, p3);
+        positions.add(3, p4);
 
         distances = new ArrayList<Double>(4);
-        for (int i =0;i<4;i++)
+        for (int i = 0; i < 4; i++)
         {
-            distances.add(i,0.0);
+            distances.add(i, 0.0);
         }
 
     }
 
-    public void updateDistances(Collection<Beacon> beacons)
+    public void updateBeacons(Collection<Beacon> beacons)
     {
-        for (Beacon b :
-                beacons)
+        int weights = 0;
+        for (BeaconEntity be :
+                this.beacons)
         {
-            if (b.getId3().toInt() == beaconID1)
+            for (Beacon b :
+                    beacons)
             {
-                distances.set(0,b.getDistance());
+                if(b.getId3().toInt() == be.getMinor())
+                {
+                    be.updateDistance(b.getDistance());
+                    be.updateWeight(b.getTxPower());
+                }
+
             }
-            else if (b.getId3().toInt() == beaconID2)
-            {
-                distances.set(1,b.getDistance());
-            }
-            else if (b.getId3().toInt() == beaconID3)
-            {
-                distances.set(2,b.getDistance());
-            }
-            else if (b.getId3().toInt() == beaconID4)
-            {
-                distances.set(3,b.getDistance());
-            }
+            weights += be.getWeight();
         }
+
 
     }
 
-    public Vector trilaterateFourBeacons(Collection<Beacon> beacons)
+
+    /*public Vector trilaterateFourBeacons()
     {
         if (positions.size() >= 4)
         {
@@ -95,17 +102,17 @@ public class TrilaterationTool {
             Vector t4 = trilaterate(positions.get(1), distances.get(1), positions.get(2), distances.get(2), positions.get(3), distances.get(3));
 
             Vector zwischenWert = t1.plus(t2).plus(t3).plus(t4);
-            lastTrilateration = zwischenWert.mal(1/4);
+            lastTrilateration = zwischenWert.mal(1 / 4);
 
             for (Double d :
                     distances)
             {
-                Log.i("Distance  ", ""+d.toString());
+                Log.i("Distance  ", "" + d.toString());
             }
             for (Vector v :
                     positions)
             {
-                Log.i("position  ", ""+v.toString());
+                Log.i("position  ", "" + v.toString());
             }
             Log.i("Trilateration 1 ", "  " + t1.toString());
             Log.i("Trilateration 2 ", "  " + t2.toString());
@@ -115,10 +122,10 @@ public class TrilaterationTool {
 
         }
         return lastTrilateration;
-    }
+    }*/
 
     //Standpunktberechnung anhand einer gegebenen Collection von Beacons
-    public Vector trilaterateThreeBeacons(Collection<Beacon> beacons)
+    /*public Vector trilaterateThreeBeacons(Collection<Beacon> beacons)
     {
 
         List<Vector> positions = new ArrayList<Vector>();
@@ -149,10 +156,41 @@ public class TrilaterationTool {
         Vector w = new Vector(-v.y, v.x);
         double t = (Math.pow(dC, 2) - Math.pow(dA, 2) - Math.pow(C.x, 2) + Math.pow(A.x, 2) - Math.pow(C.y, 2) + Math.pow(A.y, 2) - r.x * 2 * (A.x - C.x) - r.y * 2 * (A.y - C.y)) / (2 * (w.x * (A.x - C.x) + w.y * (A.y - C.y)));
         return r.plus(w.mal(t));
-    }
-    public Vector getPosition(Beacon beacon)
+    }*/
+
+
+
+    public Vector trilaterateHeuristic()
     {
-        return beaconMap.get(beacon.getId3().toInt());
+        double x, y;
+        double xMax = (beacons.get(1).getPosition().x - beacons.get(0).getPosition().x + beacons.get(2).getPosition().x - beacons.get(3).getPosition().x) / 2;
+        double yMax = (beacons.get(3).getPosition().y - beacons.get(0).getPosition().y + beacons.get(2).getPosition().y - beacons.get(1).getPosition().y) / 2;
+        double a = beacons.get(0).getDistance(), b = beacons.get(1).getDistance(), c = beacons.get(2).getDistance(), d = beacons.get(3).getDistance();
+        int weightA= beacons.get(0).getWeight(),weightB= beacons.get(1).getWeight(),weightC= beacons.get(2).getWeight(),weightD= beacons.get(3).getWeight();
+
+
+        double xVerhältnis = (weightD*(d / (c + d)) + weightA*(a / (b + a)) + weightA*(a / (c + a)) + weightD*(d / (d + b))) / (weightA *2 + weightD *2);
+        x = xVerhältnis * xMax;
+
+        double yVerhältnis = (weightA*(a / (d + a)) + weightB*(b / (b + c)) + weightB*(b / (d + b)) + weightA*(a / (a + c))) / (weightA * 2 + weightB *2);
+        y = yVerhältnis * yMax;
+
+
+        Log.i("xVerhältnis", "" + xVerhältnis);
+        Log.i("YVerhältnis", "" + yVerhältnis);
+        Log.i("Stuff", a + "   " + b + "    " + c + "    " + d);
+
+
+        Log.i("x;y", "x:" + x + "   y:" + y);
+        lastTrilateration = new Vector(x, y);
+        return lastTrilateration;
+
     }
 
+    /*public Vector getPosition(Beacon beacon)
+    {
+        return beaconMap.get(beacon.getId3().toInt());
+    }*/
+
 }
+
