466e38cb848ee69e193550533a1a89ccd3506576
ChrisMarxDev <dev@christopher-marx.de>
2016-05-12 17:34:57 +0200

    SortedBeacons für bestimmung des Quadranten

--- App/iBeaconApp/MyiBeaconApplication/app/src/main/java/com/cyberland/felix/myibeaconapplication/Trilateration/BeaconEntity.java
+++ App/iBeaconApp/MyiBeaconApplication/app/src/main/java/com/cyberland/felix/myibeaconapplication/Trilateration/BeaconEntity.java
@@ -1,11 +1,13 @@
 package com.cyberland.felix.myibeaconapplication.Trilateration;
 
+import android.util.Log;
+
 import org.altbeacon.beacon.Beacon;
 
 /**
  * Created by Studium on 10.05.2016.
  */
-public class BeaconEntity {
+public class BeaconEntity implements Comparable<BeaconEntity>{
     private Vector position;
     private int minor;
     private Beacon beacon;
@@ -47,5 +49,20 @@ public class BeaconEntity {
     public void updateWeight(int weight)
     {
         this.weight=weight;
+        Log.i("BeaconSignal","Beacon id"+ identifier + " signal: " + weight);
+        Log.i("BeaconSignal","Beacon id"+ identifier +" distance: " + distance);
+    }
+
+    @Override
+    public int compareTo(BeaconEntity another)
+    {
+        double d=another.getDistance();
+        if (d > distance)
+        {
+            return -1;
+        }
+        else {
+            return 1;
+        }
     }
 }
