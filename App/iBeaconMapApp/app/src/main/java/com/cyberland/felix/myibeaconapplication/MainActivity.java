35ed17209090c7e9178ba9f83b927deb8de8771f
Rasmus Warrelmann <Rasmus@dhcp-172-21-3-69.wlan.uni-hamburg.de.local>
2016-05-12 13:43:02 +0200

    iBeaconMapApp hinzugefügt

---
+++ App/iBeaconMapApp/app/src/main/java/com/cyberland/felix/myibeaconapplication/MainActivity.java
@@ -0,0 +1,248 @@
+package com.cyberland.felix.myibeaconapplication;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.RemoteException;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.cyberland.felix.myibeaconapplication.Trilateration.TrilaterationTool;
+import com.cyberland.felix.myibeaconapplication.Trilateration.Vector;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+
+
+public class MainActivity extends Activity implements BeaconConsumer {
+    protected static final String TAG = "MainActivity";
+    private BeaconManager beaconManager;
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+
+    TextView DistanzView1;
+    TextView DistanzView2;
+    TextView DistanzView3;
+    TextView DistanzView4;
+    TextView DistanzView5;
+    TextView localisationValue;
+
+    public String distanz1;
+    public String distanz2;
+    public String distanz3;
+    public String distanz4;
+    public String distanz5;
+    private double numDistanz2;
+    private double numDistanz3;
+    private double numDistanz4;
+    public String localisationString;
+
+    public TrilaterationTool trilaterationTool;
+
+    private BluetoothAdapter mBluetoothAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("This app needs location access");
+                builder.setMessage("Please grant location access so this app can detect beacons");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @TargetApi(Build.VERSION_CODES.M)
+                    @Override
+                    public void onDismiss(DialogInterface dialog)
+                    {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                    }
+                });
+                builder.show();
+            }
+        }
+
+        setContentView(R.layout.activity_main);
+
+        DistanzView1 = (TextView) findViewById(R.id.d1);
+        DistanzView2 = (TextView) findViewById(R.id.d2);
+        DistanzView3 = (TextView) findViewById(R.id.d3);
+        DistanzView4 = (TextView) findViewById(R.id.d4);
+        DistanzView5 = (TextView) findViewById(R.id.d5);
+        localisationValue = (TextView) findViewById(R.id.localisation);
+
+        trilaterationTool = new TrilaterationTool();
+
+
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        beaconManager.bind(this);
+
+
+        try
+        {
+            PackageInfo info = getPackageManager().getPackageInfo(this.getPackageName(), PackageManager.GET_PERMISSIONS);
+            Log.d(TAG, "SDK " + Build.VERSION.SDK_INT + " App Permissions:");
+            if (info.requestedPermissions != null)
+            {
+                for (String p : info.requestedPermissions)
+                {
+                    int grantResult = this.checkPermission(p, android.os.Process.myPid(), android.os.Process.myUid());
+                    if (grantResult == PackageManager.PERMISSION_GRANTED)
+                    {
+                        Log.d(TAG, p + " PERMISSION_GRANTED");
+                    } else
+                    {
+                        Log.d(TAG, p + " PERMISSION_DENIED: " + grantResult);
+                    }
+                }
+            }
+        } catch (Exception e)
+        {
+            Log.d(TAG, "Cannot get permissions due to error", e);
+        }
+
+
+    }
+
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        beaconManager.unbind(this);
+
+    }
+
+
+    @Override
+    public void onBeaconServiceConnect()
+    {
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(final Collection<Beacon> beacons, Region region)
+            {
+                Log.i(TAG,"BEacons in collection"+ beacons.size());
+                if (beacons.size() > 0)
+                {
+                    for (Beacon b :
+                            beacons)
+                    {
+                        Log.i(TAG, "Minor: " + b.getId3());
+                        if(b.getId3().toInt() == trilaterationTool.beaconID1)
+                        {
+                            distanz1 = (""+b.getDistance()).substring(0,6);
+                        }
+                        if(b.getId3().toInt() == trilaterationTool.beaconID2)
+                        {
+                            distanz2 = (""+b.getDistance()).substring(0,6);
+                            numDistanz2 = b.getDistance();
+                        }
+                        if(b.getId3().toInt() == trilaterationTool.beaconID3)
+                        {
+                            distanz3 = (""+b.getDistance()).substring(0,6);
+                            numDistanz3 = b.getDistance();
+                        }
+                        if(b.getId3().toInt() == trilaterationTool.beaconID4)
+                        {
+                            distanz4 = (""+b.getDistance()).substring(0,6);
+                            numDistanz4 = b.getDistance();
+                        }
+                    }
+                    trilaterationTool.updateDistances(beacons);
+                    localisationString= trilaterationTool.trilaterateFourBeacons(beacons).toString();
+                    localisationString= new Vector(numDistanz3/(numDistanz2+numDistanz3)*7,0.0).toString();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run()
+                        {
+                            DistanzView1.setText(distanz1);
+                            DistanzView2.setText(distanz2);
+                            DistanzView3.setText(distanz3);
+                            DistanzView4.setText(distanz4);
+                            DistanzView5.setText(distanz5);
+                            localisationValue.setText(localisationString);
+
+                        }
+                    });
+
+
+                }
+            }
+        });
+
+        try
+        {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e)
+        {
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults)
+    {
+        switch (requestCode)
+        {
+            case PERMISSION_REQUEST_COARSE_LOCATION:
+            {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    Log.d(TAG, "coarse location permission granted");
+                } else
+                {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                        @Override
+                        public void onDismiss(DialogInterface dialog)
+                        {
+                        }
+
+                    });
+                    builder.show();
+                }
+                return;
+            }
+        }
+    }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+
+    public void neueMessung(View view)
+    {
+        distanz1 = "";
+        distanz2 = "";
+        distanz3 = "";
+        distanz4 = "";
+        distanz5 = "";
+    }
+}
---
+++ App/iBeaconMapApp/app/src/main/java/com/cyberland/felix/myibeaconapplication/Trilateration/TrilaterationTool.java
@@ -0,0 +1,159 @@
+package com.cyberland.felix.myibeaconapplication.Trilateration;
+
+
+import android.util.Log;
+
+import org.altbeacon.beacon.Beacon;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Studium on 26.04.2016.
+ */
+public class TrilaterationTool {
+    public final int beaconID1, beaconID2, beaconID3, beaconID4;
+
+    private Vector p1, p2, p3, p4;
+
+    public Map<Integer, Vector> beaconMap;
+
+
+    Vector lastTrilateration;
+    List<Vector> positions;
+    List<Double> distances;
+
+    public TrilaterationTool()
+    {
+        p1 = new Vector(6.5, 0);
+        p2 = new Vector(7, 4.8);
+        p3 = new Vector(0, 4.8);
+        p4 = new Vector(0, 0);
+
+        beaconID1 = 24286; //Felix Beacon esti 008
+        beaconID2 = 1744; //esti003
+        beaconID3 = 21333; //esti005
+        beaconID4 = 31883; //esti002
+        //beaconID4 = 41230; //elo e10
+
+        beaconMap = new HashMap<>();
+        beaconMap.put(beaconID1, p1);
+        beaconMap.put(beaconID2, p2);
+        beaconMap.put(beaconID3, p3);
+        beaconMap.put(beaconID4, p4);
+
+        lastTrilateration = new Vector(0, 0);
+
+        positions = new ArrayList<Vector>(4);
+        positions.add(0,p1);
+        positions.add(1,p2);
+        positions.add(2,p3);
+        positions.add(3,p4);
+
+        distances = new ArrayList<Double>(4);
+        for (int i =0;i<4;i++)
+        {
+            distances.add(i,0.0);
+        }
+
+    }
+
+    public void updateDistances(Collection<Beacon> beacons)
+    {
+        for (Beacon b :
+                beacons)
+        {
+            if (b.getId3().toInt() == beaconID1)
+            {
+                distances.set(0,b.getDistance());
+            }
+            else if (b.getId3().toInt() == beaconID2)
+            {
+                distances.set(1,b.getDistance());
+            }
+            else if (b.getId3().toInt() == beaconID3)
+            {
+                distances.set(2,b.getDistance());
+            }
+            else if (b.getId3().toInt() == beaconID4)
+            {
+                distances.set(3,b.getDistance());
+            }
+        }
+
+    }
+
+    public Vector trilaterateFourBeacons(Collection<Beacon> beacons)
+    {
+        if (positions.size() >= 4)
+        {
+            Vector t1 = trilaterate(positions.get(0), distances.get(0), positions.get(1), distances.get(1), positions.get(2), distances.get(2));
+            Vector t2 = trilaterate(positions.get(0), distances.get(0), positions.get(1), distances.get(1), positions.get(3), distances.get(3));
+            Vector t3 = trilaterate(positions.get(0), distances.get(0), positions.get(2), distances.get(2), positions.get(3), distances.get(3));
+            Vector t4 = trilaterate(positions.get(1), distances.get(1), positions.get(2), distances.get(2), positions.get(3), distances.get(3));
+
+            Vector zwischenWert = t1.plus(t2).plus(t3).plus(t4);
+            lastTrilateration = zwischenWert.mal(1/4);
+
+            for (Double d :
+                    distances)
+            {
+                Log.i("Distance  ", ""+d.toString());
+            }
+            for (Vector v :
+                    positions)
+            {
+                Log.i("position  ", ""+v.toString());
+            }
+            Log.i("Trilateration 1 ", "  " + t1.toString());
+            Log.i("Trilateration 2 ", "  " + t2.toString());
+            Log.i("Trilateration 3 ", "  " + t3.toString());
+            Log.i("Trilateration 4 ", "  " + t4.toString());
+            Log.i("Last Trilateration", "  " + lastTrilateration.toString());
+
+        }
+        return lastTrilateration;
+    }
+
+    //Standpunktberechnung anhand einer gegebenen Collection von Beacons
+    public Vector trilaterateThreeBeacons(Collection<Beacon> beacons)
+    {
+
+        List<Vector> positions = new ArrayList<Vector>();
+        List<Double> distances = new ArrayList<Double>();
+
+        for (Beacon b :
+                beacons)
+        {
+            if (beaconMap.containsKey(b.getId3().toInt()))
+            {
+                positions.add(getPosition(b));
+                distances.add(b.getDistance());
+            }
+        }
+        if (positions.size() >= 3)
+        {
+            lastTrilateration = trilaterate(positions.get(0), distances.get(0), positions.get(1), distances.get(1), positions.get(2), distances.get(2));
+        }
+        return lastTrilateration;
+    }
+
+
+    public static Vector trilaterate(Vector A, double dA, Vector B, double dB, Vector C, double dC)
+    {
+        Vector v = B.minus(A);
+        double y = Math.pow(dA, 2) / (2 * v.laenge) - Math.pow(dB, 2) / (2 * v.laenge) + v.laenge / 2;
+        Vector r = v.mal(y / v.laenge);
+        Vector w = new Vector(-v.y, v.x);
+        double t = (Math.pow(dC, 2) - Math.pow(dA, 2) - Math.pow(C.x, 2) + Math.pow(A.x, 2) - Math.pow(C.y, 2) + Math.pow(A.y, 2) - r.x * 2 * (A.x - C.x) - r.y * 2 * (A.y - C.y)) / (2 * (w.x * (A.x - C.x) + w.y * (A.y - C.y)));
+        return r.plus(w.mal(t));
+    }
+    public Vector getPosition(Beacon beacon)
+    {
+        return beaconMap.get(beacon.getId3().toInt());
+    }
+
+}
