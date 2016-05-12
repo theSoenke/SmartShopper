d376ed10c284d127d6eefb047ac3cf94c954425b
theSoenke <soenke.behrendt@outlook.com>
2016-05-12 22:18:53 +0200

    Hash passwords with bcrypt

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -7,8 +7,8 @@ exports.registerUser = function (req, res, next) {
   let credentials = basicAuth(req)
 
   let user = User({
-    username: credentials.username,
-    password: credentials.password
+    username: credentials.name,
+    password: credentials.pass
   })
 
   user.save(function (err) {
@@ -16,7 +16,8 @@ exports.registerUser = function (req, res, next) {
       return next(err)
     }
 
-    res.json({status: 'User registered'})
+    res.json(user)
+  // res.json({status: 'User registered'})
   })
 }
 
@@ -24,7 +25,7 @@ exports.requireAuthentication = function (req, res, next) {
   let credentials = basicAuth(req)
 
   if (!credentials || !credentials.name || !credentials.pass) {
-    let err = new Error('Authentication failed')
+    let err = new Error('Invalid credentials')
     err.statusCode = 401
     return next(err)
   }
