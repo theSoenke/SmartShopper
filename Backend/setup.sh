e10f55d076a67df4848f5e1e4a5df37dd4623b8e
theSoenke <soenke.behrendt@outlook.com>
2016-05-14 10:09:24 +0200

    Add bcrypt dependencies

--- Backend/setup.sh
+++ Backend/setup.sh
@@ -6,6 +6,7 @@ if [ ! -d "/var/www" ]
 then
   apt-get update
   apt-get install -y curl git
+  apt-get install -y python make g++ # bcrypt dependencies
   curl -sL https://deb.nodesource.com/setup_6.x | bash -
   apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
   echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
