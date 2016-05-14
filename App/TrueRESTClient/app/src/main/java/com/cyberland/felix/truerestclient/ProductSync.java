3b641fc7056714b115adfd89920ab73b76b12c74
theSoenke <soenke.behrendt@outlook.com>
2016-05-14 10:07:09 +0200

    Async connection

--- App/TrueRESTClient/app/src/main/java/com/cyberland/felix/truerestclient/ProductSync.java
+++ App/TrueRESTClient/app/src/main/java/com/cyberland/felix/truerestclient/ProductSync.java
@@ -1,6 +1,7 @@
 package com.cyberland.felix.truerestclient;
 
-import android.accounts.AuthenticatorException;
+import android.os.AsyncTask;
+import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,35 +12,32 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ConnectException;
 import java.net.HttpURLConnection;
+import java.net.ProtocolException;
 import java.net.URL;
-import java.util.ArrayList;
 
 /**
  * Created by Felix on 12.05.2016.
  */
-public class ProductSync
+public class ProductSync extends AsyncTask<String, String, String>
 {
-    public static String getLists() throws IOException,JSONException,AuthenticatorException
-    {
-        HttpURLConnection httpURLConnection = null;
+    @Override
+    protected String doInBackground(String... params) {
+        HttpURLConnection urlConnection = null;
         BufferedReader bufferedReader = null;
-        String jsonString;
-        String path = "http://139.59.138.49:3000/lists";
+        String jsonString = "";
         int responseCode;
-
 
         try
         {
-            URL url = new URL(path);
-            httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-            httpURLConnection.setRequestProperty("Accept", "application/json");
-            httpURLConnection.setConnectTimeout(10000);
-            httpURLConnection.setReadTimeout(15000);
-            httpURLConnection.setRequestMethod("GET");
-            httpURLConnection.connect();
+            URL url = new URL(params[0]);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            urlConnection.setConnectTimeout(5000);
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
 
-            InputStream inputStream = httpURLConnection.getInputStream();
+            InputStream inputStream = urlConnection.getInputStream();
             StringBuilder builder = new StringBuilder();
             bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
@@ -50,20 +48,25 @@ public class ProductSync
             }
             jsonString = builder.toString();
 
-            responseCode = httpURLConnection.getResponseCode();
-
+            responseCode = urlConnection.getResponseCode();
             JSONObject json = new JSONObject(jsonString);
 
             if (!json.getBoolean("success"))
             {
                 throw new ConnectException();
             }
-        }
-        finally
+
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } finally
         {
-            if (httpURLConnection != null)
+            if (urlConnection != null)
             {
-                httpURLConnection.disconnect();
+                urlConnection.disconnect();
 
             }
 
@@ -75,14 +78,18 @@ public class ProductSync
                 }
                 catch (final IOException e)
                 {
-
                 }
 
             }
 
         }
 
-
         return jsonString;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+
+        Log.v("json", result);
     }
 }
